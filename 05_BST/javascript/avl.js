4e67a2713bdf8db9bf59ca2be127b52ca2b4d500
Kevin <kevinmoore9@users.noreply.github.com>
2017-04-12 16:00:50 -0700

    BST

---
+++ 05_BST/javascript/avl.js
@@ -0,0 +1,33 @@
+class AVLTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.subtrees = [null, null];
+    this.balance = 0;
+  }
+}
+
+class AVLTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+
+  }
+
+  insertUnbounded(value) {
+
+  }
+
+  remove(value) {
+
+  }
+
+  removeUnbounded(value) {
+
+  }
+
+  height() {
+
+  }
+}
