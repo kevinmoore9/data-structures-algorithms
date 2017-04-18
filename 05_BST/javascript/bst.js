aaf86e2b00ba11509f3141e28d6159ebf1635ed6
Kevin <kevinmoore9@users.noreply.github.com>
2017-04-18 15:18:50 -0700

    update BST skeleton

---
+++ 05_BST/javascript/bst.js
@@ -0,0 +1,17 @@
+class BSTNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    
+  }
+}
