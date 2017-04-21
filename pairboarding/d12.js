960f0fb81054da3bac56eefab262a8128a0633f9
Kevin <kevinmoore9@users.noreply.github.com>
2017-04-21 15:59:37 -0700

    finish d12

--- pairboarding/d12.js
+++ pairboarding/d12.js
@@ -22,3 +22,24 @@ function leftMostNode(node) {
   }
   return node;
 }
+
+
+function isBalanced (node) {
+  return isBalancedNode(node).isBalanced;
+}
+
+function isBalancedNode (node) {
+  if (!node) {
+    return {isBalanced: true, depth: -1};
+  }
+
+  let left = isBalancedNode(node.left);
+  let right = isBalancedNode(node.right);
+
+  if (left.isBalanced && right.isBalanced &&
+        Math.abs(left.depth - right.depth) <= 1) {
+    return {isBalanced: true, depth: Math.max(right.depth, left.depth) + 1};
+  } else {
+    return {isBalanced: false, depth: 0};
+  }
+}
