682910acbedf306d46764f3a90b7cf73cd46adf3
Kevin <kevinmoore9@users.noreply.github.com>
2017-06-05 14:43:13 -0700

    setup game.rb

---
+++ pairboarding/tic_tac_toe_ai/game.rb
@@ -0,0 +1,25 @@
+require_relative 'board'
+require_relative 'human_player'
+
+class TicTacToe
+  attr_reader :players, :board, :turn
+
+  def initialize(player1, player2)
+
+  end
+
+  def render_board
+
+  end
+
+  def play
+  end
+
+  private
+
+  def play_turn
+  end
+
+  def place_mark
+  end
+end
