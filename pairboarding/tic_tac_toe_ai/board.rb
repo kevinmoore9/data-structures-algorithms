85cb02d75e3f6e875ceda50f2579ce74aa32b483
Kevin <kevinmoore9@users.noreply.github.com>
2017-06-05 14:29:29 -0700

    board.rb

---
+++ pairboarding/tic_tac_toe_ai/board.rb
@@ -0,0 +1,87 @@
+class Board
+  attr_reader :rows
+
+  def initialize
+    @rows = Array.new { Array.new(3) }
+  end
+
+  def [](pos)
+    row, col = pos
+
+    @rows[row][col]
+  end
+
+  def []=(mark, pos)
+    raise "Mark already there" unless self[pos].nil?
+    row, col = pos
+
+    @rows[row][col] = mark
+  end
+
+  def columns
+    cols = []
+    0.upto(2) do |i|
+      col = []
+      0.upto(2) do |j|
+        pos = [j, i]
+        col << self[pos]
+      end
+      cols << col
+    end
+    cols
+  end
+
+  def diagonals
+    up = [[0, 0], [1, 1], [2, 2]]
+    down = [[2, 0], [1, 1], [0, 2]]
+
+    [up, down].map do |diag|
+      diag.map { |pos| self[pos] }
+    end
+  end
+
+  def next_mark
+    num_marks(:o) > num_marks(:x) ? :o : :x
+  end
+
+  def num_marks(symbol)
+    count = 0
+
+    @rows.each do |row|
+      row.each do |mark|
+        count += 1 if mark == symbol
+      end
+    end
+    count
+  end
+
+  def available_moves
+    moves = []
+    @rows.each_with_index do |row, row_idx|
+      row.each do |mark, col_idx|
+        moves << [row_idx, col_idx] unless mark
+      end
+    end
+    moves
+  end
+
+  def winner
+    (rows + diagonals + columns).each do |triple|
+      return :x if triple == [:x, :x, :x]
+      return :o if triple == [:o, :o, :o]
+    end
+
+    nil
+  end
+
+  def tied?
+    return false if !winner.nil?
+
+    @rows.all?(&:all?)
+  end
+
+  def over?
+    !winner.nil? || tied?
+  end
+
+end
---
+++ pairboarding/tic_tac_toe_ai/human_player.rb
@@ -0,0 +1,31 @@
+class HumanPlayer
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def move(game, mark)
+    ## render boaard -- game
+
+    loop do
+      puts "Please select a space"
+
+      input = gets.chomp
+      row, col = input[0].to_i, input[1].to_i
+      pos = [row, col]
+
+      if valid_input?(pos)
+        return pos
+      else
+        puts "Invalid input!"
+      end
+    end
+  end
+
+  private
+
+  def valid_input?(pos)
+    pos.all? { |coord| (0..2).include?(coord) }
+  end
+end
