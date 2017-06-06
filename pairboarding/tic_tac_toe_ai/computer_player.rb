cfa2e48bce0d57c1444e9381f37bdf03fe064459
Kevin <kevinmoore9@users.noreply.github.com>
2017-06-05 23:28:00 -0700

    basic AI: make and block winning moves

--- pairboarding/tic_tac_toe_ai/computer_player.rb
+++ pairboarding/tic_tac_toe_ai/computer_player.rb
@@ -1,18 +1,36 @@
 class ComputerPlayer
   attr_reader :name
 
-
   def iniaialize
     @name = "SuperComputer"
   end
 
   def move(game, symbol)
+    winning_move(game, symbol) ||
+      block_winning_move(game, symbol) ||
       random_move(game)
   end
 
   private
 
+
+
+  def winning_move(game, symbol)
+    game.board.available_moves.each do |move|
+      dup_board = game.board.dup
+      dup_board[move] = symbol
+      return move if dup_board.winner == symbol
+
+    end
+    nil
+  end
+
   def random_move(game)
     game.board.available_moves.sample
+  end
+
+  def block_winning_move(game, symbol)
+    symbol = (symbol == :x ? :o : :x)
+    winning_move(game, symbol)
   end
 end
